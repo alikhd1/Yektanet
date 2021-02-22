bb174c4ef258a78b2f04a8b36e925bf4f057e4d4
Ali khandi <alikhandi@gmail.com>
2021-02-23 00:58:51 +0330

    added tracking middleware

--- advertising/views/index.py
+++ advertising/views/index.py
@@ -10,15 +10,15 @@ class IndexView(TemplateView):
     advertisers = Advertiser.objects.all()
 
     @classmethod
-    def increaseView(cls):
+    def increaseView(cls, ip):
         for advertiser in cls.advertisers:
             for ad in advertiser.ads.all():
-                tracker = EventTracking(ad=ad, event='vi')
+                tracker = EventTracking(ad=ad, event='vi', visitor_ip=ip)
                 tracker.save()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.increaseView()
+        self.increaseView(ip=kwargs.get('ip', '127.0.0.1'))
         context['advertisers'] = self.advertisers
 
         return context
@@ -28,7 +28,7 @@ class AdView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         ad = get_object_or_404(Ad, pk=kwargs['ad_id'])
-        tracker = EventTracking(ad=ad, event='ck')
+        tracker = EventTracking(ad=ad, event='ck', ip=kwargs.get('ip', '127.0.0.1'))
         tracker.save()
 
         self.url = ad.link
